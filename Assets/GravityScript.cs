6ea5273a5de49a622f2c7c62df7271b2246cc098
ZLTM <jdavidrevillad@gmail.com>
2019-05-20 18:03:07 -0400

    initial commit
    Initial project uploaded
    *Ball working by changing tilt
    *objects generated

---
+++ Assets/GravityScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityScript : MonoBehaviour
+{
+
+    public float grav = 380;
+
+    // Use this for initialization
+    void Start()
+    {
+        //CameraDevice.Instance.StopAutoFocus();
+        if (Screen.orientation == ScreenOrientation.LandscapeLeft)
+        {
+            Debug.Log("LEFT");
+
+        }
+        else if (Screen.orientation == ScreenOrientation.LandscapeRight)
+        {
+            Debug.Log("RIGHT");
+        }
+        else if (Screen.orientation == ScreenOrientation.Portrait)
+        {
+            Debug.Log("PORTRAIT");
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+        //CameraDevice.Instance.StopAutoFocus();
+        /*if (Screen.orientation == ScreenOrientation.LandscapeLeft) {
+			Physics.gravity = new Vector3 (Input.acceleration.y * grav , -Input.acceleration.x * grav , -Input.acceleration.z * grav);
+		} else if (Screen.orientation == ScreenOrientation.LandscapeRight ) {
+			Physics.gravity = new Vector3 (-Input.acceleration.y * grav , Input.acceleration.x * grav , -Input.acceleration.z * grav);
+		} else if (Screen.orientation == ScreenOrientation.Portrait ) {*/
+        Physics.gravity = new Vector3(Input.acceleration.x * grav, Input.acceleration.y * grav, -Input.acceleration.z * grav);
+        //}
+    }
+}
---
+++ Assets/ballScript.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballScript : MonoBehaviour
+{
+    public float XTilt = 90f;
+    public float NewXTilt;
+    public float YTilt = 0f;
+    public float NewYTilt;
+    public GameObject spawnPoint;
+
+    public float MovementSpeed;
+
+    private void Update()
+    {
+
+        if (NewXTilt <= XTilt)
+        {
+            print("Tilted -z moving up");
+            transform.Translate(Vector3.forward * MovementSpeed * Time.deltaTime);
+        }
+
+        else if (NewXTilt >= XTilt)
+        {
+            print("Tilted -z moving down");
+            transform.Translate(-Vector3.forward * MovementSpeed * Time.deltaTime);
+        }
+
+        else 
+        {
+            print(" -z not moving");
+        }
+
+        if (NewYTilt <= YTilt)
+        {
+            print("Tilted -x moving right");
+            transform.Translate(Vector3.right * MovementSpeed * Time.deltaTime);
+        }
+
+        else if (NewYTilt >= YTilt)
+        {
+            print("Tilted -x moving left");
+            transform.Translate(Vector3.left * MovementSpeed * Time.deltaTime);
+        }
+
+        else
+        {
+            print(" -y not moving");
+        }
+    }
+
+    void OnTriggerEnter(Collider Other)
+    {
+
+        if (Other.gameObject.tag == "Respawn")
+        {
+            transform.position = spawnPoint.transform.position;
+        }
+        
+    }
+}
